480a15fd80b902c4e524e92e1fdbd0669f1f863e
labyu <labyu2020@gmail.com>
2020-12-05 02:07:34 +0900

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  configureWebpack: { // other webpack options to merge in ... }, 
+  devServer: { 
+    host: '0.0.0.0',
+    hot: false,
+    disableHostCheck: true,
+  }, 
+};
