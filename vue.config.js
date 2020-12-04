8221aea6e2943f81db5a9625683ec6c0c367bf12
labyu <labyu2020@gmail.com>
2020-12-05 02:08:16 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  configureWebpack: { // other webpack options to merge in ... }, 
+  configureWebpack: { 
+    // other webpack options to merge in ... 
+  }, 
   devServer: { 
     host: '0.0.0.0',
     hot: false,
